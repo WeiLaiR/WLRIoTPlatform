34443ac7dc3353b42cdd1d23392532f075d3bfb2
Wei <1397311537@qq.com>
2022-12-20 19:45:55 +0800

    新增CoAP服务异步消息发送功能

--- DeviceGateway/src/main/java/com/wei/devicegateway/server/CoAPDeviceServer.java
+++ DeviceGateway/src/main/java/com/wei/devicegateway/server/CoAPDeviceServer.java
@@ -5,17 +5,33 @@ import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.coap.CoAP;
 import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CoAPDeviceServer {
-    private static void coapServer() {
+
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private void setRabbitTemplate(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    private void coapServer() {
+
         CoapServer coapServer = new CoapServer(6667);
         coapServer.add(new CoapResource("coapTelemetry") {
             @Override
             public void handlePOST(CoapExchange exchange) {
-                String requestText = exchange.getRequestText();
-                System.out.println(requestText);
+                String message = exchange.getRequestText();
+                System.out.println(message);
+
+                String exchangeName = "device.direct";
+                String routingKey = "coap";
+
+                rabbitTemplate.convertAndSend(exchangeName,routingKey,message + "RoutingKeyIs:" + routingKey);
+
                 exchange.respond(CoAP.ResponseCode.CONTENT, "OKK");
             }
         });
@@ -24,7 +40,7 @@ public class CoAPDeviceServer {
 
     @PostConstruct
     public void startCoapServer() {
-        new Thread(CoAPDeviceServer::coapServer).start();
+        new Thread(this::coapServer).start();
 
         System.out.println("===========================CoAP服务器已开启===========================");
     }
