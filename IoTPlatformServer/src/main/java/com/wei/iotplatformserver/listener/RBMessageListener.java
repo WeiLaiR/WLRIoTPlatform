56d6dbb2b25b9306c170f652b7f1ffd979b9dac8
Wei <1397311537@qq.com>
2022-12-20 19:47:27 +0800

    服务端创建RabbitMQ消息监听

---
+++ IoTPlatformServer/src/main/java/com/wei/iotplatformserver/listener/RBMessageListener.java
@@ -0,0 +1,41 @@
+package com.wei.iotplatformserver.listener;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RBMessageListener {
+
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "device.http.queue"),
+            exchange = @Exchange(name = "device.direct", type = ExchangeTypes.DIRECT),
+            key = {"http"}
+    ))
+    public void ListenDirectExchangeQueueHttpMessage(String msg) {
+        System.out.println("接收到HttpQueue的消息" + msg);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "device.mqtt.queue1"),
+            exchange = @Exchange(name = "device.direct", type = ExchangeTypes.DIRECT),
+            key = {"mqtt"}
+    ))
+    public void ListenDirectExchangeQueueMqttMessage(String msg) {
+        System.out.println("接收到MqttQueue的消息" + msg);
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "device.coap.queue1"),
+            exchange = @Exchange(name = "device.direct", type = ExchangeTypes.DIRECT),
+            key = {"coap"}
+    ))
+    public void ListenDirectExchangeQueueCoAPMessage(String msg) {
+        System.out.println("接收到CoAPQueue的消息" + msg);
+    }
+
+}
