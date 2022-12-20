5bf493306c492c1157a33e0f5258d6720576f547
Wei <1397311537@qq.com>
2022-12-20 18:17:23 +0800

    新增测试

--- DeviceGateway/src/test/java/com/wei/devicegateway/DeviceGatewayApplicationTests.java
+++ DeviceGateway/src/test/java/com/wei/devicegateway/DeviceGatewayApplicationTests.java
@@ -1,13 +1,76 @@
 package com.wei.devicegateway;
 
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.elements.exception.ConnectorException;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootTest
 class DeviceGatewayApplicationTests {
 
     @Test
-    void contextLoads() {
+    void coapTest() throws URISyntaxException, ConnectorException, IOException {
+        URI uri = new URI("localhost:6667/coapTelemetry");
+        CoapClient client = new CoapClient(uri);
+        CoapResponse response = client.post("123123123", MediaTypeRegistry.TEXT_PLAIN);
+        if(response != null){
+            System.out.println(response.getCode());  //打印请求状态码
+            System.out.println(response.getOptions());  //选项参数
+            System.out.println(response.getResponseText());  //获取内容文本信息
+            System.out.println("\nAdvanced\n");    //
+            System.out.println(Utils.prettyPrint(response));  //打印格式良好的输出
+        }
+    }
+
+    @Test
+    void mqttTest() {
+        String broker = "tcp://192.168.3.66:1886";
+        String topic = "mqtt/weilai";
+        String username = "weilai";
+        String password = "weilaiPassword";
+        String clientid = "publish_client_weilai";
+        String content = "Test Message 123123123";
+        int qos = 0;
+
+        try {
+            MqttClient client = new MqttClient(broker, clientid, new MemoryPersistence());
+            // 连接参数
+            MqttConnectOptions options = new MqttConnectOptions();
+            // 设置用户名和密码
+            options.setUserName(username);
+            options.setPassword(password.toCharArray());
+            options.setConnectionTimeout(5);
+            options.setKeepAliveInterval(60);
+            options.setAutomaticReconnect(true);
+            // 连接
+            client.connect(options);
+            // 创建消息并设置 QoS
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            // 发布消息
+            client.publish(topic, message);
+            System.out.println("Message published");
+            System.out.println("topic: " + topic);
+            System.out.println("message content: " + content);
+            // 关闭连接
+            client.disconnect();
+            // 关闭客户端
+            client.close();
+        } catch (MqttException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
