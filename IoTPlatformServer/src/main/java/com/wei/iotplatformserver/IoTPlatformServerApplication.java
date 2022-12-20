3ee2efe619dad97684c8b2349ada2cd261ab0be2
Wei <1397311537@qq.com>
2022-12-20 19:44:07 +0800

    服务端启动文件

---
+++ IoTPlatformServer/src/main/java/com/wei/iotplatformserver/IoTPlatformServerApplication.java
@@ -0,0 +1,13 @@
+package com.wei.iotplatformserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTPlatformServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTPlatformServerApplication.class, args);
+    }
+
+}
