da618480ffe7b6cda971e555492eecae459afe95
Wei <1397311537@qq.com>
2022-12-20 17:22:28 +0800

    项目搭建

---
+++ DeviceGateway/src/main/java/com/wei/devicegateway/DeviceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.wei.devicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceGatewayApplication.class, args);
+    }
+
+}
---
+++ DeviceGateway/src/test/java/com/wei/devicegateway/DeviceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.devicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
