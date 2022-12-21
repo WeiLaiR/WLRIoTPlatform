a3f69359a2241fe8114729096de4b5209937952d
Wei <1397311537@qq.com>
2022-12-21 11:20:58 +0800

    项目变更。

---
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/IoTPlatformServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wei.iotplatformservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTPlatformServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTPlatformServiceApplication.class, args);
+    }
+
+}
---
+++ IoTPlatformService/src/test/java/com/wei/iotplatformservice/IoTPlatformServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.iotplatformservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTPlatformServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
