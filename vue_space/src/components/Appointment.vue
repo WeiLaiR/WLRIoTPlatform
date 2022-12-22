3c16af3392591abd9dad87e26d553897015307be
Wei <1397311537@qq.com>
2022-12-22 14:36:04 +0800

    编写图标展示

---
+++ vue_space/src/components/Appointment.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card height="" flat color="transparent" class="mt-n5">
+                <v-toolbar color="rgba(0,0,0,0)" flat class="py-4">
+                  <v-toolbar-title class=""> Appointment </v-toolbar-title>
+                </v-toolbar>
+
+                <polar-chart
+                  :chart-id="chartId"
+                  :width="width"
+                  height="200px"
+                  :css-classes="cssClasses"
+                  :styles="styles"
+                  :plugins="plugins"
+                />
+              </v-card>
+</template>
+
+<script>
+import PolarChart from "../chart/PolarChart.js";
+export default {
+    data:  () => ({
+         props: {
+      chartId: {
+        type: String,
+        default: "polar-chart",
+      },
+      width: {
+        type: Number,
+        default: 400,
+      },
+      height: {
+        type: Number,
+        default: 100,
+      },
+      cssClasses: {
+        default: "",
+        type: String,
+      },
+      styles: {
+        type: Object,
+        default: () => {},
+      },
+      plugins: {
+        type: Array,
+        default: () => {},
+      },
+    },
+   
+    }),
+     components: {
+    PolarChart,
+   
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_space/src/components/History.vue
@@ -0,0 +1,59 @@
+<template>
+   <v-card height="" flat color="transparent" class="mt-n5">
+                <v-toolbar color="rgba(0,0,0,0)" flat class="py-4">
+                  <v-toolbar-title class=""> History Survey </v-toolbar-title>
+                </v-toolbar>
+
+                <line-chart
+                  :chart-id="chartId"
+                  :width="width"
+                  height="200px"
+                  :css-classes="cssClasses"
+                  :styles="styles"
+                  :plugins="plugins"
+                />
+              </v-card>
+</template>
+
+<script>
+import LineChart from "../chart/LineChart.js";
+export default {
+    data:  () => ({
+         props: {
+      chartId: {
+        type: String,
+        default: "line-chart",
+      },
+      width: {
+        type: Number,
+        default: 400,
+      },
+      height: {
+        type: Number,
+        default: 100,
+      },
+      cssClasses: {
+        default: "",
+        type: String,
+      },
+      styles: {
+        type: Object,
+        default: () => {},
+      },
+      plugins: {
+        type: Array,
+        default: () => {},
+      },
+    },
+   
+    }),
+     components: {
+    LineChart,
+   
+  },
+}
+</script>
+
+<style>
+
+</style>
