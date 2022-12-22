0f3497ce904c81b3031402af2cbb5645b622b34e
Wei <1397311537@qq.com>
2022-12-22 14:36:38 +0800

    编写平台页面模板

---
+++ vue_space/src/views/Manage.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <v-app
+      id="inspire"
+      :style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+
+    <SideBar />
+
+    <v-divider color="white" class="mt-1" dark></v-divider>
+
+    <router-view />
+
+  </v-app>
+
+</template>
+
+<script>
+
+import SideBar from "../components/SideBar";
+export default {
+  name: "Manage",
+
+
+  components: {
+    SideBar,
+  },
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
