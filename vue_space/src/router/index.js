0937f31b45f70c84a0fd6661d7f48fc44dc8b857
Wei <1397311537@qq.com>
2023-01-13 14:10:33 +0800

    修改路由规则

--- vue_space/src/router/index.js
+++ vue_space/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Manage from "@/views/Manage.vue";
+import Login from "@/views/Login.vue";
+import Register from "@/views/Register.vue";
 
 Vue.use(VueRouter)
 
@@ -21,6 +23,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: "/login",
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register
   }
 ]
 
