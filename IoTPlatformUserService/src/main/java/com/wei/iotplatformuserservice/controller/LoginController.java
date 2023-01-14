f84628e208f7fea8756e8ff5c77c772ab101d892
Wei <1397311537@qq.com>
2023-01-14 17:21:32 +0800

    完成部分用户登录功能

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.wei.iotplatformuserservice.controller;
+
+import com.wei.iotplatformuserservice.pojo.Login;
+import com.wei.iotplatformuserservice.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user/login")
+public class LoginController {
+
+    private LoginService loginService;
+    @Autowired
+    public void setLoginService(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/sign_in")
+    public Map<String, Object> SignIn(@RequestBody Login login) {
+//        System.out.println(login);
+        return loginService.signIn(login.getEmail(), login.getPassword());
+    }
+}
