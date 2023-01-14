0a2c99cdca3b68e5d758929270c3a0c9e74b851a
Wei <1397311537@qq.com>
2023-01-14 11:39:40 +0800

    用户服务初始化

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wei.iotplatformuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTPlatformUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTPlatformUserServiceApplication.class, args);
+    }
+
+}
---
+++ IoTPlatformUserService/src/test/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.iotplatformuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTPlatformUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
