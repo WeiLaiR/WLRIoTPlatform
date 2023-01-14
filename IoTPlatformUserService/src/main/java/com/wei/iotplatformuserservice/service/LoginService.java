4f4bb87425a5d5c1a463e0dcbb32d83ceec0e496
Wei <1397311537@qq.com>
2023-01-14 17:20:20 +0800

    编写Login部分接口

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/service/LoginService.java
@@ -0,0 +1,14 @@
+package com.wei.iotplatformuserservice.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wei.iotplatformuserservice.pojo.Login;
+
+import java.util.Map;
+
+public interface LoginService extends IService<Login> {
+
+    Map<String, Object> signIn(String email, String password);
+
+    Map<String, Object> deleteLoginInfo(Long uid);
+
+}
