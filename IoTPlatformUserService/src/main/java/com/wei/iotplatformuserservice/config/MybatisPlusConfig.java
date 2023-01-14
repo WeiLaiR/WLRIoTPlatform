8a0a572838e3662a23de4adf582d2313fe2daed0
Wei <1397311537@qq.com>
2023-01-14 17:19:47 +0800

    配置MybatisPlus

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.wei.iotplatformuserservice.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.wei.iotplatformuserservice.mapper")
+public class MybatisPlusConfig {
+
+//    配置分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
