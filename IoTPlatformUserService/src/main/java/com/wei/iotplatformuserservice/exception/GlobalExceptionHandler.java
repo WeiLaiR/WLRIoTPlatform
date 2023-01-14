5fa2edacdde8d007be050c9106f5d59fde979419
Wei <1397311537@qq.com>
2023-01-14 11:43:11 +0800

    设置全局异常

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.wei.iotplatformuserservice.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 全局异常
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ResponseBody
+    @ExceptionHandler(CustomException.class)
+    public Map<String, Object> handleCustomException(CustomException customException) {
+        Map<String, Object> errorResultMap = new HashMap<>(16);
+        errorResultMap.put("state", customException.getState());
+        errorResultMap.put("message", customException.getMessage());
+        return errorResultMap;
+    }
+}
