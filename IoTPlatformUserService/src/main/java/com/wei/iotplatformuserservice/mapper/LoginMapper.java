d5bcf968b5f4eb3b171346bd9910080a7634487e
Wei <1397311537@qq.com>
2023-01-14 17:19:13 +0800

    创建LoginMapper

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/mapper/LoginMapper.java
@@ -0,0 +1,9 @@
+package com.wei.iotplatformuserservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wei.iotplatformuserservice.pojo.Login;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginMapper extends BaseMapper<Login> {
+}
