eaaede5ac69a1fd251327334876c1e48d9ad31b2
Wei <1397311537@qq.com>
2023-01-14 11:43:22 +0800

    设置自定义异常

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/exception/CustomException.java
@@ -0,0 +1,21 @@
+package com.wei.iotplatformuserservice.exception;
+
+import lombok.Getter;
+
+/**
+ * 自定义异常
+ * 输入两个String
+ * state是状态
+ * message是消息
+ */
+@Getter
+public class CustomException extends RuntimeException {
+    private String state;
+    private String message;
+
+    public CustomException(String code, String message) {
+        this.state = code;
+        this.message = message;
+    }
+
+}
