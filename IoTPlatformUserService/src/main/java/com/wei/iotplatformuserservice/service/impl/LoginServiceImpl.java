0f64e8bcdee928816f8c8acfa97457a8218db3ac
Wei <1397311537@qq.com>
2023-01-15 11:58:35 +0800

    更新登录流程

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/service/impl/LoginServiceImpl.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/service/impl/LoginServiceImpl.java
@@ -5,6 +5,9 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wei.iotplatformuserservice.mapper.LoginMapper;
 import com.wei.iotplatformuserservice.pojo.Login;
 import com.wei.iotplatformuserservice.service.LoginService;
+import com.wei.iotplatformuserservice.utils.CreateRsaKey;
+import com.wei.iotplatformuserservice.utils.RedisUtil;
+import com.wei.iotplatformuserservice.utils.TokenUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,10 @@ import java.util.Map;
 
 @Service
 public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login> implements LoginService {
+
+    //    设置token过期时间，单位为秒，当前为3天
+    private static final long TIMEOUT = 259200;
+
     private LoginMapper loginMapper;
 
     @Autowired
@@ -20,14 +27,37 @@ public class LoginServiceImpl extends ServiceImpl<LoginMapper, Login> implements
         this.loginMapper = loginMapper;
     }
 
+    private CreateRsaKey createRsaKey;
+
+    @Autowired
+    public void setCreateRsaKey(CreateRsaKey createRsaKey) {
+        this.createRsaKey = createRsaKey;
+    }
+
+    private RedisUtil redisUtil;
+
+    @Autowired
+    public void setRedisUtil(RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
+    }
+
     @Override
     public Map<String, Object> signIn(String email, String password) {
+//        获取私钥
+        String privateKey = createRsaKey.getPrivateKey();
+        System.out.println(privateKey);
+
         Map<String, Object> map = new HashMap<>();
         QueryWrapper<Login> wrapper = new QueryWrapper<>();
         wrapper.eq("email", email);
         Login login = loginMapper.selectOne(wrapper);
         if (login != null) {
             if (login.getPassword().equals(password)) {
+//                生成Token并存入redis
+                String token = TokenUtils.getToken(login.getUid(), login.getEmail());
+                redisUtil.set(login.getUid().toString(), token);
+
+                map.put("token", token);
                 map.put("status", 200);
                 map.put("message", "登录成功");
             } else {
