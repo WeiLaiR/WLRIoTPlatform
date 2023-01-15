8bb319c2fee7c99ab1ae7a61a7c62a1bd973757a
Wei <1397311537@qq.com>
2023-01-15 19:29:00 +0800

    测试

--- MailService/src/test/java/com/wei/mailservice/MailServiceApplicationTests.java
+++ MailService/src/test/java/com/wei/mailservice/MailServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.wei.mailservice;
 
+import com.wei.mailservice.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MailServiceApplicationTests {
 
+    private RedisUtil redisUtil;
+
+    @Autowired
+    public void setRedisUtil(RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
+    }
     @Test
     void contextLoads() {
+
+    }
+    @Test
+    void redisTest() {
+        System.out.println(redisUtil.get("@"));
     }
 
 }
