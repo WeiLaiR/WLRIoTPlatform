41af44e679478eba12efcff5ba14dee2d2c1a057
Wei <1397311537@qq.com>
2023-01-15 11:29:39 +0800

    用于发送公钥

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/RsaController.java
@@ -0,0 +1,32 @@
+package com.wei.iotplatformuserservice.controller;
+
+
+import com.wei.iotplatformuserservice.utils.CreateRsaKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user/ras")
+public class RsaController {
+    private CreateRsaKey rsa;
+
+    @Autowired
+    public void setRsa(CreateRsaKey rsa) {
+        this.rsa = rsa;
+    }
+
+
+    @GetMapping("/getPublicKey")
+    public Map<String, Object> getPublicKey() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("publicKey", rsa.getPublicKey());
+        return map;
+    }
+
+
+}
