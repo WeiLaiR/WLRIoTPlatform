9cf5771521cd5152144e745251fe03b7bc572d79
Wei <1397311537@qq.com>
2023-01-15 11:59:56 +0800

    新增Mail服务

---
+++ MailService/src/test/java/com/wei/mailservice/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.mailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
