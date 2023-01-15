e0465aaebbc3b7913bcc3864e2a179bf38faa0e2
Wei <1397311537@qq.com>
2023-01-15 19:30:07 +0800

    生成验证码

---
+++ MailService/src/main/java/com/wei/mailservice/utils/BuildVCode.java
@@ -0,0 +1,37 @@
+package com.wei.mailservice.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BuildVCode {
+
+    //    设置token过期时间，单位为秒，当前为五分钟
+    private static final long TIMEOUT = 300;
+
+    //    验证码可再次请求时间
+    private static final long TRYAGAIN = 120;
+
+
+    private RedisUtil redisUtil;
+
+    @Autowired
+    public void setRedisUtil(RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
+    }
+
+
+    /**
+     * 生成验证码,并存入Redis
+     *
+     * @return 验证码
+     */
+    public String buildVCode(String eMail) {
+        String randomCode = Integer.toString((int) ((Math.random() * 9 + 1) * 100000));
+
+        redisUtil.set(eMail, randomCode, TIMEOUT);
+        redisUtil.set(eMail + "0ta", "0", TRYAGAIN);
+
+        return randomCode;
+    }
+}
