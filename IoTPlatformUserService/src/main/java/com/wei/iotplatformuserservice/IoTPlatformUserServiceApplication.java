16066296eacfe3272bc88590d5fb5663769f1a7e
Wei <1397311537@qq.com>
2023-01-15 11:30:12 +0800

    开始Spring定时任务

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplication.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplication.java
@@ -2,7 +2,9 @@ package com.wei.iotplatformuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling //开启定时任务
 @SpringBootApplication
 public class IoTPlatformUserServiceApplication {
 
