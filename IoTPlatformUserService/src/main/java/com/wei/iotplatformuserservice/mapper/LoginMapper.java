7d3210f9f0e07daeba4cd1e343faad0d5d33ae4c
Wei <1397311537@qq.com>
2023-01-16 19:10:44 +0800

    新增查询并完成相关SQL语句编写

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/mapper/LoginMapper.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/mapper/LoginMapper.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LoginMapper extends BaseMapper<Login> {
     Login queryLogin(String email);
+
+
+    String queryEmailEmpty(String email);
 }
