31c6855b99c98a8b4e4853cfd7f6d0fe4f761a89
Wei <1397311537@qq.com>
2023-01-16 19:13:31 +0800

    构建DeviceDataNumber实体类

---
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/pojo/DeviceDataNumber.java
@@ -0,0 +1,24 @@
+package com.wei.iotplatformservice.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeviceDataNumber {
+    //    使用了自定义id生成器
+    @TableId(type = IdType.ASSIGN_ID)
+    private Long deviceDataId;
+    private Long deviceCfgId;
+    private String dataNumber;
+    //    注解优先级高于yml配置文件中的设置！
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+}
