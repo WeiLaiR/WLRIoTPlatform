18bb36ca7ce82be7761acaef85efde2c38ca2d73
Wei <1397311537@qq.com>
2023-01-16 19:12:16 +0800

    重构id生成规则，已线程安全

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/utils/CustomIdGenerator.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/utils/CustomIdGenerator.java
@@ -1,36 +1,49 @@
 package com.wei.iotplatformuserservice.utils;
 
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Component
 public class CustomIdGenerator implements IdentifierGenerator {
 
     private static long time = new Date().getTime();
     //    计数位
-    private static long count = 0;
+    private final AtomicLong count = new AtomicLong(0);
+
+    //      机器码,通过配置文件配置。
+    @Value("${machine.code}")
+    public static long mac;
 
     @Override
     public Long nextId(Object entity) {
-        long id = new Date().getTime();
-        System.out.println(id);
-        System.out.println(time);
-//      机器码,不会自动生成，所以用手设置。
-        long mac = 6;
+        long now;
 
-        if (id == time) {
-            count++;
+        if (count.get() < 99) {
+            count.addAndGet(1);
+            now = time;
         } else {
-            time = id;
-            count = 0;
+            now = new Date().getTime();
+//            这里仅锁住对象即可，没有必要锁住整个类
+            synchronized (this) {
+                if (now == time) {
+                    do {
+                        now = new Date().getTime();
+                    } while (now == time);
+                    time = now;
+                }
+            }
+            count.set(0);
         }
+
 //        乘10默认机器码一位十进制，可设置0-9
-        id = id * 10 + mac;
+        now = now * 10 + mac;
 //        乘100默认计数位两位十进制，可设置0-99
-        id = id * 100 + count;
+        now = now * 100 + count.get();
         //返回生成的id值即可.
-        return id;
+        return now;
     }
 }
