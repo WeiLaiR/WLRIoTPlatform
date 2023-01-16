e57f77a7ce39c2a0250df85cc8c7edd1d15614a0
Wei <1397311537@qq.com>
2023-01-16 13:37:27 +0800

    测试

--- IoTPlatformUserService/src/test/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplicationTests.java
+++ IoTPlatformUserService/src/test/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.wei.iotplatformuserservice;
 
+import com.wei.iotplatformuserservice.mapper.LoginMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IoTPlatformUserServiceApplicationTests {
 
+    @Autowired
+    private LoginMapper loginMapper;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void Test1() {
+        System.out.println(loginMapper.queryLogin("1@a.com"));
     }
 
 }
