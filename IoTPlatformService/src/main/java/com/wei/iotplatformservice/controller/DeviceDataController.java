d4e051a8691b4efaff4caf6c4ce8cf9ba4897fc0
Wei <1397311537@qq.com>
2023-01-17 14:35:23 +0800

    Controller层初始化

---
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/controller/DeviceDataController.java
@@ -0,0 +1,9 @@
+package com.wei.iotplatformservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/device/data")
+public class DeviceDataController {
+}
