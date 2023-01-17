6efc90454d666310ede31411b374b9760ec5f716
Wei <1397311537@qq.com>
2023-01-17 15:45:34 +0800

    处理跨域

---
+++ IoTPlatformGateway/src/main/java/com/wei/iotplatformgateway/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.wei.iotplatformgateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+
+/**
+ * 解决跨域问题
+ */
+
+@Configuration
+public class CorsConfig {
+
+    /**
+     * 处理跨域
+     * @return 1
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(259200L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
+
+
+}
