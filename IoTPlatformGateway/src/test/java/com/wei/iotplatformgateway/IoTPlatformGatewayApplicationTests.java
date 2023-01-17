5139cede92e8191bcfc79ca1792ff04f7474f168
Wei <1397311537@qq.com>
2023-01-17 15:44:08 +0800

    导入网关等依赖

---
+++ IoTPlatformGateway/src/test/java/com/wei/iotplatformgateway/IoTPlatformGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wei.iotplatformgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTPlatformGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
