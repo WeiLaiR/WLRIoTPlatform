74ecd8db7c4906ce6a33fcdfa82f24b2deb0e6ad
Wei <1397311537@qq.com>
2023-01-17 14:35:06 +0800

    创建DeviceInfoController并完成新增设备接口

---
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/controller/DeviceInfoController.java
@@ -0,0 +1,30 @@
+package com.wei.iotplatformservice.controller;
+
+import com.wei.iotplatformservice.pojo.DeviceInfo;
+import com.wei.iotplatformservice.service.DeviceInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/device/info")
+public class DeviceInfoController {
+
+    private DeviceInfoService deviceInfoService;
+
+    @Autowired
+    public void setDeviceInfoService(DeviceInfoService deviceInfoService) {
+        this.deviceInfoService = deviceInfoService;
+    }
+
+    @PostMapping("/add")
+    public Map<String, Object> newDeviceInfo(@RequestBody DeviceInfo deviceInfo) {
+        return deviceInfoService.newDeviceInfo(deviceInfo);
+    }
+
+
+}
