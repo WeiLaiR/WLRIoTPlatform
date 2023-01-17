2a5c74d8e726f090b1b043c0fcc92d00f28f886c
Wei <1397311537@qq.com>
2023-01-17 14:34:02 +0800

    全局异常和自定义异常

---
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/exception/CustomException.java
@@ -0,0 +1,21 @@
+package com.wei.iotplatformservice.exception;
+
+import lombok.Getter;
+
+/**
+ * 自定义异常
+ * 输入两个String
+ * state是状态
+ * message是消息
+ */
+@Getter
+public class CustomException extends RuntimeException {
+    private Object status;
+    private String message;
+
+    public CustomException(Object code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+
+}
