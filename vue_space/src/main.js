ce0d13057b0f54e6682109b2aeea3a69d5217257
Wei <1397311537@qq.com>
2023-01-31 17:31:50 +0800

    导入axios、vue-axios、request

--- vue_space/src/main.js
+++ vue_space/src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import VueAxios from "vue-axios";
+import request from "@/utils/request";
+
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios,axios)
+Vue.prototype.request=request
 
 new Vue({
   router,
