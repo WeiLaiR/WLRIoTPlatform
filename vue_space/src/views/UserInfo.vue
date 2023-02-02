77ddcc3502742e8278f62c45a0f6036921d26342
Wei <1397311537@qq.com>
2023-02-02 12:51:31 +0800

    创建用户信息页面

---
+++ vue_space/src/views/UserInfo.vue
@@ -0,0 +1,327 @@
+<template>
+  <div style="height: 100%">
+    <v-main style="margin: 0;padding: 0;height: 100%">
+
+      <div style="margin: 10px;padding-left: 15px">
+        <div class="teal--text" style="font-size: 26px">用户信息</div>
+      </div>
+      <v-divider style="margin-left: 7px;margin-right: 7px"></v-divider>
+
+
+      <v-card
+          max-width="90%"
+          min-width="96%"
+          min-height="88%"
+          elevation="3"
+          style="margin: 20px auto;background: #f8f9f9"
+          outlined
+      >
+
+        <div>
+
+          <v-row>
+
+            <v-col
+                cols="6"
+            >
+              <v-text-field
+                  label="输入用户id或邮箱"
+                  filled
+                  style="margin-left: 30px;margin-top: 22px;"
+                  color="#2ebfaf"
+                  append-icon="mdi-magnify"
+                  v-model="deviceSearch">
+
+              </v-text-field>
+
+            </v-col>
+
+            <v-col
+                cols="3"
+            >
+
+              <!--       空位置       -->
+
+            </v-col>
+
+            <v-col
+                cols="3"
+            >
+              <v-btn right
+                     absolute
+                     elevation="5"
+                     large rounded
+                     color="#2ebfaf"
+                     dark
+                     style="display:inline;margin-top: 27px;margin-right: 15px"
+              >
+                CREATE mUUID
+              </v-btn>
+
+            </v-col>
+
+          </v-row>
+
+
+        </div>
+
+
+        <div>
+
+          <v-data-table
+              :headers="headers"
+              :items="desserts"
+              :items-per-page="rowNums"
+              :page="pageNum"
+              :server-items-length="itemNums"
+              class="elevation-1"
+              style="margin-left: 30px;margin-right: 30px"
+          >
+
+            <!--      弹出的修改框      -->
+            <template v-slot:top>
+              <v-dialog v-model="dialog" max-width="500px">
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field v-model="editedItem.name" label="Device ID"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field v-model="editedItem.calories" label="Device Name"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field v-model="editedItem.fat" label="Create Time"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field v-model="editedItem.carbs" label="Description"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field v-model="editedItem.protein" label="Status"></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                    <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </template>
+
+            <!--      两个按键      -->
+
+            <template v-slot:item.actions="{ item }">
+              <v-icon
+                  small
+                  color="#2ebfaf"
+                  class="mr-2"
+                  @click="editItem(item)"
+              >
+                mdi-pencil
+              </v-icon>
+
+              <v-icon
+                  small
+                  color="#ff3f6f"
+                  @click="deleteItem(item)"
+              >
+                mdi-delete
+              </v-icon>
+            </template>
+
+
+          </v-data-table>
+
+        </div>
+
+
+
+      </v-card>
+
+
+
+    </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserInfo",
+  data() {
+    return {
+      deviceSearch: "",
+      rowNums: 10,
+      pageNum: 1,
+      itemNums: 10,
+
+
+      dialog: false,
+      headers: [
+        {
+          text: '用户ID',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: '用户名', value: 'calories' , sortable: false},
+        { text: '邮箱', value: 'fat' , sortable: false},
+        { text: '账户创建时间', value: 'carbs' , sortable: false},
+        { text: '电话', value: 'protein' , sortable: false},
+        { text: '操作', value: 'actions', sortable: false },
+      ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+      defaultItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+
+    }
+  },
+
+
+  // 监听dialog的变化，当dialog为false时，关闭对话框
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    // deviceSearch (newVal, oldVal) {
+    //   console.log('newVal' + newVal + 'OLD' + oldVal);
+    // }
+  },
+
+  // 启动后触发此触发器填充假数据
+  created () {
+    this.initialize()
+  },
+
+  methods: {
+    initialize () {
+      this.desserts = [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+        },
+      ]
+    },
+
+    editItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      const index = this.desserts.indexOf(item)
+      confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
+      }
+      this.close()
+    },
+
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
