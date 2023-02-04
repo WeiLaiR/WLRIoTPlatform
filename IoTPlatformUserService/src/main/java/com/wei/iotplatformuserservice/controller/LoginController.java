31eda1edbe9392723ad1c0cf00b0b653677aad08
Wei <1397311537@qq.com>
2023-02-04 17:45:49 +0800

    路径变更

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/LoginController.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/user/login")
 public class LoginController {
 
     private LoginService loginService;
--- MailService/src/main/java/com/wei/mailservice/controller/MailController.java
+++ MailService/src/main/java/com/wei/mailservice/controller/MailController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/email")
+@RequestMapping("/mail/email")
 public class MailController {
 
 
