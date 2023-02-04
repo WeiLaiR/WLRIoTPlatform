d0e7b9e06f81b8d407d365f05e9b786fb48e3f6c
Wei <1397311537@qq.com>
2023-02-04 17:44:56 +0800

    路径变更

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/RsaController.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/RsaController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/ras")
+@RequestMapping("/user/ras")
 public class RsaController {
     private CreateRsaKey rsa;
 
--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/UserController.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user/users")
 public class UserController {
     private UserService userService;
 
