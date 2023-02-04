cc4a53f91ddaefffa9d62d487c0dd00956bb5208
Wei <1397311537@qq.com>
2023-02-04 17:48:15 +0800

    导入并注册相关组件

--- vue_space/src/main.js
+++ vue_space/src/main.js
@@ -6,12 +6,21 @@ import vuetify from './plugins/vuetify'
 import axios from 'axios'
 import VueAxios from "vue-axios";
 import request from "@/utils/request";
+import Message from "@/components/message/index.js";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faTriangleExclamation} from '@fortawesome/free-solid-svg-icons'
+import {faCircleCheck} from '@fortawesome/free-solid-svg-icons'
+// import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 
 Vue.config.productionTip = false
 
 Vue.use(VueAxios,axios)
 Vue.prototype.request=request
+Vue.prototype.$message = Message
+library.add(faTriangleExclamation, faCircleCheck)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
