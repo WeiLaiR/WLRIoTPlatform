1c0b2808bccaae3ced85ef5750d8721bf27d0327
Wei <1397311537@qq.com>
2023-02-06 17:05:17 +0800

    开启定时任务

--- MailService/src/main/java/com/wei/mailservice/MailServiceApplication.java
+++ MailService/src/main/java/com/wei/mailservice/MailServiceApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient //注册到服务器中心
 @EnableFeignClients //开启Feign
 @EnableAsync //开启异步
+@EnableScheduling //开启定时任务
 public class MailServiceApplication {
 
     public static void main(String[] args) {
