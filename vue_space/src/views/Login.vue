8b16106deb87d000a2af9a91e1526db0da77298b
Wei <1397311537@qq.com>
2023-02-06 17:12:14 +0800

    完善登录模块，引入多重加密

--- vue_space/src/views/Login.vue
+++ vue_space/src/views/Login.vue
@@ -90,18 +90,22 @@
 
                     </v-text-field>
 
+
+
+                    <v-card-actions
+                    >
+
+                      <v-btn outlined color="#2ebfaf" style="margin: 0 auto" @click="login">Log In</v-btn>
+
+                      <v-btn outlined color="#2ebfaf" style="margin: 0 auto" @click="signup">Sign up</v-btn>
+
+                    </v-card-actions>
+
                   </v-form>
 
                 </v-card-actions>
 
-                <v-card-actions
-                >
 
-                  <v-btn outlined color="#2ebfaf" style="margin: 0 auto" @click="login">Log In</v-btn>
-
-                  <v-btn outlined color="#2ebfaf" style="margin: 0 auto" @click="signup">Sign up</v-btn>
-
-                </v-card-actions>
 
               </v-col>
 
@@ -119,6 +123,7 @@
 
 <script>
 import request from "@/utils/request";
+import JSEncrypt from "jsencrypt";
 
 export default {
   name: "Login",
@@ -145,21 +150,49 @@ export default {
 
   methods: {
 
-    login() {
-      request.get("/user/ras/getPublicKey").then(res => {
-        this.publicKey = res.publicKey;
-        console.log(this.publicKey);
-      })
+    async login() {
 
-      console.log("==============================");
+      if (this.$refs.loginForm.validate()) {
+        await request.get("/user/ras/getPublicKey").then(res => {
+          this.publicKey = res.publicKey;
+          console.log(this.publicKey);
+        })
 
-      request.post("/user/login/sign_in", this.Login).then(res => {
-        console.log(res);
-        if (res.status === 200) {
-          this.$router.push("/home");
-          localStorage.setItem("token", res.token);
+        console.log("==============================");
+
+
+        // 进行简单的加盐
+        let temp = ""
+        for (let i = 0; i < this.Login.password.length; i++) {
+          temp += (i + 7) * (this.Login.password.charCodeAt(this.Login.password.length - i - 1) + temp.length) % 103
+          temp += String.fromCharCode((((i + 3) * this.Login.password.charCodeAt(i) + temp.length) % this.Login.password.length) % 24 + 97)
+          temp += ((this.Login.password.charCodeAt(i) + 16) * temp.length % 107)
+          if (i * temp.length % 2 === 0) {
+            temp += String.fromCharCode(((this.Login.password.charCodeAt(this.Login.password.length - i - 1) + temp.length) * (i * temp.length % 300 + 6)) % 24 + 65)
+          }
+          temp += String.fromCharCode(((this.Login.password.charCodeAt(i) + i) * temp.length) % 24 + 65)
         }
-      })
+
+        console.log('加盐' + temp);
+
+        let sha256 = this.$SHA256(temp);
+
+        console.log('sha256加密' + sha256);
+
+        let encrypt = new JSEncrypt();
+        encrypt.setPublicKey(this.publicKey);
+        this.pwEncrypt = encrypt.encrypt(sha256);
+
+        console.log('RSA加密' + this.pwEncrypt);
+
+        request.post("/user/login/sign_in", {email: this.Login.email, password: this.pwEncrypt}).then(res => {
+          console.log(res);
+          if (res.status === 200) {
+            this.$router.push("/home");
+            localStorage.setItem("token", res.token);
+          }
+        })
+      }
 
     },
 
@@ -177,7 +210,7 @@ export default {
 .wrapper {
   height: 100vh;
   background-image: url("../assets/bg2.jpg");
-  background-size: 100%;
+  background-size: cover;
   overflow: hidden;
 }
 
