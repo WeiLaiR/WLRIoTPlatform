970e1062f1b737a33d4f9a4bf534020cd618e7a1
Wei <1397311537@qq.com>
2023-02-08 19:40:15 +0800

    完成接口功能

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/service/impl/UserServiceImpl.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/service/impl/UserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.wei.iotplatformuserservice.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wei.iotplatformuserservice.exception.CustomException;
 import com.wei.iotplatformuserservice.mapper.UserMapper;
 import com.wei.iotplatformuserservice.pojo.User;
 import com.wei.iotplatformuserservice.service.UserService;
+import com.wei.iotplatformuserservice.utils.TokenUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -24,6 +27,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     @Override
     public Map<String, Object> searchUserInfo(Long uid) {
         Map<String, Object> map = new HashMap<>();
+        if (uid == null) {
+            uid = TokenUtils.getId();
+            if (uid == null) {
+                throw new CustomException(400, "出现了未知错误，无法获取用户信息！");
+            }
+        }
         User user = userMapper.selectById(uid);
         if (user != null) {
             map.put("status", 200);
@@ -33,7 +42,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
             map.put("status", 400);
             map.put("message", "用户不存在");
         }
-
         return map;
     }
 
@@ -47,6 +55,44 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
             map.put("status", 400);
             map.put("message", "删除失败");
         }
+        return map;
+    }
+
+    @Override
+    public Map<String, Object> updateUserInfo(User user) {
+        Map<String, Object> map = new HashMap<>();
+        if (user.getUid() == null) {
+            user.setUid(TokenUtils.getId());
+        }
+        if (userMapper.updateById(user) > 0) {
+            map.put("status", 200);
+            map.put("message", "个人信息保存成功！");
+        } else {
+            map.put("status", 400);
+            map.put("message", "数据更新失败，请稍后再试！");
+        }
+        return map;
+    }
+
+    @Override
+    public Map<String, Object> queryUserListP(Integer start, Integer limit, String val) {
+        Map<String, Object> map = new HashMap<>();
+        List<User> users;
+        int count;
+        if (val.equals("null")) {
+            users = userMapper.qUserListPage(start, limit);
+            count = userMapper.qUserListCount();
+        }else {
+            val = val.concat("%");
+            users = userMapper.qUserListPQbc(val, start, limit);
+            count = userMapper.qUserListCountQbc();
+        }
+
+        map.put("status", 200);
+        map.put("message", "查询成功");
+        map.put("data", users);
+        map.put("total", count);
+
         return map;
     }
 }
