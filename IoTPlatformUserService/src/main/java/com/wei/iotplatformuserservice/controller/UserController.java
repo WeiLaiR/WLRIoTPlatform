81992109e85d13b36d954147885bf3b605c2290f
Wei <1397311537@qq.com>
2023-02-08 19:41:29 +0800

    完成获取、更新、分页查询用户信息等功能

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/UserController.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.wei.iotplatformuserservice.controller;
 
+import com.wei.iotplatformuserservice.pojo.User;
 import com.wei.iotplatformuserservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,5 +26,26 @@ public class UserController {
     public Map<String, Object> getUserInfo(@PathVariable Long uid) {
         return userService.searchUserInfo(uid);
     }
+
+    /**
+     * 获取当前用户信息
+     * @return 1
+     */
+    @GetMapping("/userinfo")
+    public Map<String, Object> getMyInfo() {
+        return userService.searchUserInfo(null);
+    }
+
+    @PutMapping("/update")
+    public Map<String, Object> updateUserInfo(@RequestBody User user) {
+        return userService.updateUserInfo(user);
+    }
+
+    @GetMapping("/listP/{start}/{limit}/{val}")
+    public Map<String, Object> getUserListP(@PathVariable Integer start, @PathVariable Integer limit, @PathVariable String val) {
+        return userService.queryUserListP((start - 1) * limit, limit, val);
+    }
+
+
 
 }
