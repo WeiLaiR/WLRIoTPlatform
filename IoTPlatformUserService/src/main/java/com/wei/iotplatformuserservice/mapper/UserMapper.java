556dea06679357769c763394cac3bef872f79ca0
Wei <1397311537@qq.com>
2023-02-08 19:37:18 +0800

    新增用户分页查询等mapper

--- IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/mapper/UserMapper.java
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/mapper/UserMapper.java
@@ -4,6 +4,16 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wei.iotplatformuserservice.pojo.User;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserMapper extends BaseMapper<User> {
+
+    List<User> qUserListPQbc(String val, Integer start, Integer limit);
+
+    List<User> qUserListPage(Integer start, Integer limit);
+
+    Integer qUserListCount();
+
+    Integer qUserListCountQbc();
 }
