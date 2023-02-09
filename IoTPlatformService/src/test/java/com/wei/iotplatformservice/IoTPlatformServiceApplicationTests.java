d4f55465bd7703cd427c17af16aaa7bf1eee2767
Wei <1397311537@qq.com>
2023-02-09 14:29:30 +0800

    test

--- IoTPlatformService/src/test/java/com/wei/iotplatformservice/IoTPlatformServiceApplicationTests.java
+++ IoTPlatformService/src/test/java/com/wei/iotplatformservice/IoTPlatformServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package com.wei.iotplatformservice;
 
+import com.wei.iotplatformservice.mapper.DeviceInfoMapper;
+import com.wei.iotplatformservice.pojo.DeviceInfo;
+import com.wei.iotplatformservice.utils.DeviceTokenUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IoTPlatformServiceApplicationTests {
 
+    @Autowired
+    DeviceInfoMapper deviceInfoMapper;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void Test1() {
+        for (int i = 0; i < 9999; i++) {
+            long val = 2837548679L;
+            DeviceInfo deviceInfo = new DeviceInfo();
+
+            String deviceToken = DeviceTokenUtil.BuildDeviceToken();
+
+            deviceInfo.setDeviceToken(deviceToken);
+
+            deviceInfo.setUid(1675673716886001L);
+
+            deviceInfo.setDeviceName( i + "name");
+
+            deviceInfo.setDescription(val + i + "scr");
+
+            deviceInfoMapper.insert(deviceInfo);
+        }
     }
 
 }
--- IoTPlatformUserService/src/test/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplicationTests.java
+++ IoTPlatformUserService/src/test/java/com/wei/iotplatformuserservice/IoTPlatformUserServiceApplicationTests.java
@@ -1,6 +1,8 @@
 package com.wei.iotplatformuserservice;
 
 import com.wei.iotplatformuserservice.mapper.LoginMapper;
+import com.wei.iotplatformuserservice.mapper.UserMapper;
+import com.wei.iotplatformuserservice.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,13 +13,48 @@ class IoTPlatformUserServiceApplicationTests {
     @Autowired
     private LoginMapper loginMapper;
 
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+
+
+
+
     }
 
     @Test
     void Test1() {
         System.out.println(loginMapper.queryLogin("1@a.com"));
+    }
+
+    @Test
+    void Test2() {
+        for (int i = 0; i < 9999; i++) {
+            int num = 667 + i;
+            User user = new User();
+            user.setUid((long) num);
+            user.setAge((short) 23);
+            user.setEmail(num + "@a.com");
+            user.setSex(true);
+            user.setPhoneNumber(num + "phone");
+            user.setRemarks(num + "remarks");
+            user.setUname(num + "uname");
+            user.setBriefIntroduction(num + "briefIntroduction");
+            userMapper.insert(user);
+        }
+    }
+
+    @Test
+    void Test3() {
+        System.out.println(userMapper.qUserListPQbc("%", 0, 10));
+    }
+
+    @Test
+    void Test4() {
+        System.out.println(userMapper.qUserListPage( 0, 10));
     }
 
 }
