6c1d5e2334858f00d8b2378f61151996cdd2e4e4
Wei <1397311537@qq.com>
2023-02-09 14:31:34 +0800

    创建分页查询mapper接口

--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/mapper/DeviceInfoMapper.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/mapper/DeviceInfoMapper.java
@@ -4,6 +4,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wei.iotplatformservice.pojo.DeviceInfo;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DeviceInfoMapper extends BaseMapper<DeviceInfo> {
+
+    List<DeviceInfo> qInfoListPQbc(String val, Integer start, Integer limit, Long uid);
+
+    Integer qInfoListCountQbc();
 }
