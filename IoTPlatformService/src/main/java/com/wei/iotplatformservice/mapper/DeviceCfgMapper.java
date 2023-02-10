c048f7cd48e0ebd17438d09921559409b14769f1
Wei <1397311537@qq.com>
2023-02-10 19:14:28 +0800

    完成分页查询硬件配置、查询硬件简要配置等

--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/mapper/DeviceCfgMapper.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/mapper/DeviceCfgMapper.java
@@ -4,6 +4,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wei.iotplatformservice.pojo.DeviceCfg;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DeviceCfgMapper extends BaseMapper<DeviceCfg> {
+    List<DeviceCfg> queryDeviceCfgListP(Integer start, Integer limit, Long did);
+
+    Integer queryDeviceCfgCountP();
+
+    List<DeviceCfg> queryBriefCfgList(Long did);
+
+    Boolean queryDeviceIsNumber(Long cid);
 }
