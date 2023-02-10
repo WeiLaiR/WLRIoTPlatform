b4bae72e880fff2183d4db94d61da5db406f0028
Wei <1397311537@qq.com>
2023-02-10 19:16:35 +0800

    完成分页查询数据信息接口

--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/service/DeviceDataService.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/service/DeviceDataService.java
@@ -3,5 +3,9 @@ package com.wei.iotplatformservice.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.wei.iotplatformservice.pojo.DeviceData;
 
+import java.util.Map;
+
 public interface DeviceDataService extends IService<DeviceData> {
+
+    Map<String, Object> queryDeviceDataListP(Integer start, Integer limit, Long cid, String startTime, String endTime);
 }
--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/service/impl/DeviceDataServiceImpl.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/service/impl/DeviceDataServiceImpl.java
@@ -4,9 +4,34 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wei.iotplatformservice.mapper.DeviceDataMapper;
 import com.wei.iotplatformservice.pojo.DeviceData;
 import com.wei.iotplatformservice.service.DeviceDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class DeviceDataServiceImpl extends ServiceImpl<DeviceDataMapper, DeviceData> implements DeviceDataService {
 
+    DeviceDataMapper deviceDataMapper;
+    @Autowired
+    public void setDeviceDataMapper(DeviceDataMapper deviceDataMapper) {
+        this.deviceDataMapper = deviceDataMapper;
+    }
+
+    @Override
+    public Map<String, Object> queryDeviceDataListP(Integer start, Integer limit, Long cid, String startTime, String endTime) {
+        HashMap<String, Object> map = new HashMap<>();
+
+        List<DeviceData> deviceData = deviceDataMapper.queryDeviceDataListP(start, limit, cid, startTime, endTime);
+        Integer count = deviceDataMapper.queryDeviceDataCount();
+
+        map.put("status", 200);
+        map.put("message", "查询成功");
+        map.put("data", deviceData);
+        map.put("total", count);
+
+        return map;
+    }
 }
