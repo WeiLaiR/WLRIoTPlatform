f0493e422f02eec8289924ac5f4f293868a331af
Wei <1397311537@qq.com>
2023-02-10 19:16:07 +0800

    完成分页查询数据信息

--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/mapper/DeviceDataMapper.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/mapper/DeviceDataMapper.java
@@ -4,6 +4,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wei.iotplatformservice.pojo.DeviceData;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DeviceDataMapper extends BaseMapper<DeviceData> {
+
+    List<DeviceData> queryDeviceDataListP(Integer start, Integer limit, Long cid, String startTime, String endTime);
+
+    Integer queryDeviceDataCount();
+
 }
