d6982f095271b07cc2dcb7e9f6872e99dab649b7
Wei <1397311537@qq.com>
2023-02-10 19:15:15 +0800

    完成分页查询硬件配置、查询硬件简要配置功能

--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/controller/DeviceCfgController.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/controller/DeviceCfgController.java
@@ -1,9 +1,37 @@
 package com.wei.iotplatformservice.controller;
 
+import com.wei.iotplatformservice.exception.CustomException;
+import com.wei.iotplatformservice.service.DeviceCfgService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/platform/deviceCfg")
 public class DeviceCfgController {
+    private DeviceCfgService deviceCfgService;
+    @Autowired
+    public void setDeviceCfgService(DeviceCfgService deviceCfgService) {
+        this.deviceCfgService = deviceCfgService;
+    }
+
+    @GetMapping("/listP/{start}/{limit}/{did}")
+    public Map<String, Object> getDeviceCfgListP(@PathVariable Integer start, @PathVariable Integer limit, @PathVariable Long did) {
+        if (start >= 0 && limit > 0) {
+            return deviceCfgService.queryDeviceCfgListP((start - 1) * limit, limit, did);
+        } else {
+            throw new CustomException(400, "暂不可查询所有内容！");
+        }
+    }
+
+    @GetMapping("/briefList/{did}")
+    public Map<String, Object> getBriefCfgList(@PathVariable Long did) {
+        return deviceCfgService.queryBriefCfgList(did);
+    }
+
+
 }
