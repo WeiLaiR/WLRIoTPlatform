d461eac74e360237fbbbb53f56f8e0b4cdc12481
Wei <1397311537@qq.com>
2023-02-15 17:30:54 +0800

    配置自定义异步异常

---
+++ DeviceGateway/src/main/java/com/wei/devicegateway/exception/CustomAsyncExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.wei.devicegateway.exception;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import java.lang.reflect.Method;
+
+public class CustomAsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+
+    @Override
+    public void handleUncaughtException(Throwable throwable, Method method, Object... obj) {
+        System.out.println("异常捕获---------------------------------");
+        System.out.println("Exception message - " + throwable.getMessage());
+        System.out.println("Method name - " + method.getName());
+        for (Object param : obj) {
+            System.out.println("Parameter value - " + param);
+        }
+        System.out.println("异常捕获---------------------------------");
+    }
+
+}
