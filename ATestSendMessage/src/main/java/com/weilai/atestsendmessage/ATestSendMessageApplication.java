d3bfc60ae13a61d98767250d930ad6eb101f9001
Wei <1397311537@qq.com>
2023-02-15 17:29:19 +0800

    测试项目项目初始化

---
+++ ATestSendMessage/src/main/java/com/weilai/atestsendmessage/ATestSendMessageApplication.java
@@ -0,0 +1,13 @@
+package com.weilai.atestsendmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ATestSendMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ATestSendMessageApplication.class, args);
+    }
+
+}
---
+++ ATestSendMessage/src/test/java/com/weilai/atestsendmessage/ATestSendMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weilai.atestsendmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ATestSendMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
