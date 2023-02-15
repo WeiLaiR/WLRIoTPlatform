5fd3616f7e16d9774a8ff0649cd04aa214afd4be
Wei <1397311537@qq.com>
2023-02-15 17:31:53 +0800

    CoAP发送初始化

---
+++ ATestSendMessage/src/main/java/com/weilai/atestsendmessage/coap/CoAPSendMessage.java
@@ -0,0 +1,28 @@
+package com.weilai.atestsendmessage.coap;
+
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.elements.exception.ConnectorException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@Component
+public class CoAPSendMessage {
+    private final String URL = "localhost:6667/coapTelemetry";
+
+    private CoapClient client;
+
+    @PostConstruct
+    private void init(){
+        client = new CoapClient(URL);
+    }
+    public void sendMessage(String message) {
+        try {
+            client.post(message, MediaTypeRegistry.TEXT_PLAIN);
+        } catch (ConnectorException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
