2ab2bba0d3cd4662890aab30ea609cf11cb8740c
Wei <1397311537@qq.com>
2023-02-18 19:34:29 +0800

    MQ加收到的消息统一处理

--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/listener/RBMessageListener.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/listener/RBMessageListener.java
@@ -1,53 +1,61 @@
 package com.wei.iotplatformservice.listener;
 
-import org.springframework.amqp.core.ExchangeTypes;
+import com.wei.iotplatformservice.utils.DeviceMessageHandler;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class RBMessageListener {
 
+    private DeviceMessageHandler deviceMessageHandler;
+    @Autowired
+    public void setDeviceMessageHandler(DeviceMessageHandler deviceMessageHandler) {
+        this.deviceMessageHandler = deviceMessageHandler;
+    }
 
     /**
      * 接受MQ消息
-     * @param msg http信息
+     * @param map http信息
      */
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(name = "device.http.queue"),
-            exchange = @Exchange(name = "device.direct", type = ExchangeTypes.DIRECT),
+            exchange = @Exchange(name = "device.direct"),
             key = {"http"}
     ))
-    public void ListenDirectExchangeQueueHttpMessage(String msg) {
-        System.out.println("接收到HttpQueue的消息" + msg);
+    public void ListenDirectExchangeQueueHttpMessage(Map<String, Object> map) {
+        deviceMessageHandler.handleMessage(map, "HTTP");
     }
 
     /**
      * 接受MQ消息
-     * @param msg mqtt信息
+     * @param map mqtt信息
      */
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(name = "device.mqtt.queue1"),
-            exchange = @Exchange(name = "device.direct", type = ExchangeTypes.DIRECT),
+            exchange = @Exchange(name = "device.direct"),
             key = {"mqtt"}
     ))
-    public void ListenDirectExchangeQueueMqttMessage(String msg) {
-        System.out.println("接收到MqttQueue的消息" + msg);
+    public void ListenDirectExchangeQueueMqttMessage(Map<String, Object> map) {
+        deviceMessageHandler.handleMessage(map, "MQTT");
     }
 
     /**
      * 接受MQ消息
-     * @param msg coap信息
+     * @param map coap信息
      */
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(name = "device.coap.queue1"),
-            exchange = @Exchange(name = "device.direct", type = ExchangeTypes.DIRECT),
+            exchange = @Exchange(name = "device.direct"),
             key = {"coap"}
     ))
-    public void ListenDirectExchangeQueueCoAPMessage(String msg) {
-        System.out.println("接收到CoAPQueue的消息" + msg);
+    public void ListenDirectExchangeQueueCoAPMessage(Map<String, Object> map) {
+        deviceMessageHandler.handleMessage(map, "CoAP");
     }
 
 }
