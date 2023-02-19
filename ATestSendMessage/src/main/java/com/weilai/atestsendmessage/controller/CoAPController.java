a8e187a2edb50c5054eb84cf3b04b4f2fcc697c0
Wei <1397311537@qq.com>
2023-02-19 17:40:06 +0800

    新增查询目前正在发送的设备tokenId

--- ATestSendMessage/src/main/java/com/weilai/atestsendmessage/controller/CoAPController.java
+++ ATestSendMessage/src/main/java/com/weilai/atestsendmessage/controller/CoAPController.java
@@ -3,10 +3,7 @@ package com.weilai.atestsendmessage.controller;
 import com.weilai.atestsendmessage.coap.CoAPSendMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -55,7 +52,7 @@ public class CoAPController {
     public Map<String, Object> stopCoAP(@RequestBody Map<String, Object> map) {
         Map<String, Object> map1 = new HashMap<>();
 
-        status.put((String) map.get("token"), false);
+        status.remove((String) map.get("token"));
 
         map1.put("state", "200");
         map1.put("message", "success");
@@ -63,14 +60,25 @@ public class CoAPController {
         return map1;
     }
 
+    @GetMapping("/getMap")
+    public Map<String, Object> getMap() {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("state", "200");
+        map.put("message", "success");
+        map.put("data", status);
+
+        return map;
+    }
+
 
     private void CoAPTask(String token, List<Map<String, Object>> list) {
         Random random = new Random();
         double version = ((int) (random.nextDouble() * 30)) / 10.0 + 0.1;
-        while (status.getOrDefault(token, true)) {
+        while (status.getOrDefault(token, false)) {
             StringBuilder sb = new StringBuilder();
             sb.append("token=").append(token);
-            sb.append("&version=").append(version);
+            sb.append("&version=").append(String.format("%.1f", version));
             for (Map<String, Object> val : list) {
                 int statVal = 0;
                 int endVal = 100;
