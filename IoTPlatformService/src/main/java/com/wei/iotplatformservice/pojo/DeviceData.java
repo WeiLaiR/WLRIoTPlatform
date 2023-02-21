75edbff311f53855da562f901befac1fc151f85a
Wei <1397311537@qq.com>
2023-02-21 20:11:45 +0800

    新增字段

--- IoTPlatformService/src/main/java/com/wei/iotplatformservice/pojo/DeviceData.java
+++ IoTPlatformService/src/main/java/com/wei/iotplatformservice/pojo/DeviceData.java
@@ -21,7 +21,7 @@ public class DeviceData {
     @TableId(type = IdType.ASSIGN_ID)
     private Long deviceDataId;
     private Long deviceCfgId;
-
+    private String equipmentNo;
     private String protocol;
     private String data;
     //    注解优先级高于yml配置文件中的设置！
