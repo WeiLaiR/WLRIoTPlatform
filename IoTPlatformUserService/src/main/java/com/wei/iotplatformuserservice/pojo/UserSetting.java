3a4fcb9bdfee39cabf27440d84d13e3856e58ab2
Wei <1397311537@qq.com>
2023-02-25 14:21:37 +0800

    新增UserSetting实体类

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/pojo/UserSetting.java
@@ -0,0 +1,38 @@
+package com.wei.iotplatformuserservice.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSetting {
+    //    id自行输入
+    @TableId(type = IdType.INPUT)
+    private Long uid;
+
+    private Boolean sendEmail;
+
+    private Integer keepAlive;
+
+    //    注解优先级高于yml配置文件中的设置！
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date updateTime;
+
+    //    逻辑删除
+    @TableLogic
+    //    不展示逻辑删除位
+    @JsonIgnore
+    private Boolean isDelete;
+}
