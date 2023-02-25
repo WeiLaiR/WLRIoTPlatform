567984c795bec03cb917618beabfa8521d7d546e
Wei <1397311537@qq.com>
2023-02-25 14:21:55 +0800

    创建UserSettingMapper

---
+++ IoTPlatformUserService/src/main/java/com/wei/iotplatformuserservice/mapper/UserSettingMapper.java
@@ -0,0 +1,9 @@
+package com.wei.iotplatformuserservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wei.iotplatformuserservice.pojo.UserSetting;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserSettingMapper extends BaseMapper<UserSetting> {
+}
