37b65056f7580836cc9ad9b368f1b1f1ddbe8b37
Wei <1397311537@qq.com>
2023-02-25 14:25:44 +0800

    新增警告邮件发送

--- MailService/src/main/java/com/wei/mailservice/utils/SendEmail.java
+++ MailService/src/main/java/com/wei/mailservice/utils/SendEmail.java
@@ -4,12 +4,11 @@ import com.wei.mailservice.exception.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.mail.internet.MimeMessage;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Date;
+import java.util.Map;
 
 @Component
 public class SendEmail {
@@ -28,34 +27,48 @@ public class SendEmail {
         this.buildVCode = buildVCode;
     }
 
-    private static Queue<String> queue = new LinkedList<>();
 
-    public static void addQueue(String email) {
-        queue.add(email);
+    public void senEmailUt(String email) {
+        Date date = new Date();
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        String vCode = buildVCode.buildVCode(email);
+
+        try {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+
+            messageHelper.setSubject("[验证码]——WLR IoT Platform");
+            messageHelper.setText(BuildMail.buildContent(vCode), true);
+            messageHelper.setTo(email);
+            messageHelper.setFrom("WLR IoT Platform" + '<' + "info@baiblog.top" + '>');
+
+            javaMailSender.send(mimeMessage);
+
+        } catch (Exception e) {
+            throw new CustomException(400, "邮件发送失败！请稍后重试或联系管理员。");
+        }
+        Date date1 = new Date();
+        System.out.println(date + "==================" + date1);
     }
 
 
-    @Scheduled(cron = "0/15 * * * * ?")
-    void senEmailUt() {
-        while (!queue.isEmpty()) {
-            String email = queue.poll();
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            String vCode = buildVCode.buildVCode(email);
+    public void sendWarnEmail(Map<String, Object> map) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        StringBuilder sb = new StringBuilder();
+        sb.append("您好，ID为 ").append(map.get("uid")).append(" 的用户！").append(map.get("message"));
+        try {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
 
-            try {
-                MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+            messageHelper.setSubject("[警告]——WLR IoT Platform");
+            messageHelper.setText(BuildMail.buildContent1(sb.toString()), true);
+            messageHelper.setTo(map.get("email").toString());
+            messageHelper.setFrom("WLR IoT Platform" + '<' + "info@baiblog.top" + '>');
 
-                messageHelper.setSubject("[验证码]——WLR IoT Platform");
-                messageHelper.setText(BuildMail.buildContent(vCode), true);
-                messageHelper.setTo(email);
-                messageHelper.setFrom("WLR IoT Platform" + '<' + "info@baiblog.top" + '>');
+            javaMailSender.send(mimeMessage);
 
-                javaMailSender.send(mimeMessage);
-
-            } catch (Exception e) {
-                throw new CustomException(400, "邮件发送失败！请稍后重试或联系管理员。");
-            }
+        } catch (Exception e) {
+            throw new CustomException(400, "邮件发送失败！请稍后重试或联系管理员。");
         }
     }
+
 
 }
