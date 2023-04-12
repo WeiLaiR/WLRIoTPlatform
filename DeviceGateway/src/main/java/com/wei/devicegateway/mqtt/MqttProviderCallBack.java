1fd9d3a3f1d883fa55ccb8d7c6dabd01b006811d
Wei <1397311537@qq.com>
2023-04-12 15:31:41 +0800

    配置MqttCallBack

---
+++ DeviceGateway/src/main/java/com/wei/devicegateway/mqtt/MqttProviderCallBack.java
@@ -0,0 +1,43 @@
+package com.wei.devicegateway.mqtt;
+
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqttProviderCallBack implements MqttCallback {
+
+
+    private final String clientId = "publish_client_weilai";
+
+    /**
+     * 与服务器断开的回调
+     */
+    @Override
+    public void connectionLost(Throwable cause) {
+        System.out.println(clientId + "与服务器断开连接");
+    }
+
+    /**
+     * 消息到达的回调
+     */
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+
+    }
+
+    /**
+     * 消息发布成功 回调
+     */
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        IMqttAsyncClient client = token.getClient();
+//        System.out.println(client.getClientId() + "发布消息成功！");
+
+    }
+
+}
+
